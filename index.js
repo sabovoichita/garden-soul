db2f4d313925a1c979569908cec9cb805059e088
Voichita Maria <sabo_voichita@yahoo.com>
2025-02-25 13:43:34 +0100

    createTitle, initEvents, css for title

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Welcome");
+
+function createTitle() {
+  return `<h1>The Garden Soul</h1>`;
+}
+
+function initEvents() {
+  document.body.innerHTML = createTitle();
+}
+initEvents();
